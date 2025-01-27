bae223b51c6920440f4dab0d022a102e20d7a330
Prashant-Microsoft <v-pmalusare@microsoft.com>
2025-01-27 21:05:59 +0530

    pylint fix

--- src/backend/event_utils.py
+++ src/backend/event_utils.py
@@ -2,6 +2,7 @@ import logging
 import os
 from azure.monitor.events.extension import track_event
 
+
 def track_event_if_configured(event_name: str, event_data: dict):
     instrumentation_key = os.getenv("APPLICATIONINSIGHTS_INSTRUMENTATION_KEY")
     if instrumentation_key:
