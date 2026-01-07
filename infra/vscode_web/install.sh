cfd464b0050e60c411e41905aac5f0d608e1052c
Ragini-Microsoft <v-raginich@microsoft.com>
2026-01-07 13:03:54 +0530

    Update the vs code web url for mace-v3 branch

--- infra/vscode_web/install.sh
+++ infra/vscode_web/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user -q
 
-azd init -t microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator
+azd init -t microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator -b macae-v3
